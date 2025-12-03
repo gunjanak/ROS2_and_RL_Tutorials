c8b06636c7ae588c170b0bf3fbe9997f42d4ee8f
gunjanak <janak.klal@gmail.com>
2025-12-03 07:29:43 +0545

    Tutorial 1 temp sensor simulator done

--- src/sensor_simulator/setup.py
+++ src/sensor_simulator/setup.py
@@ -5,8 +5,7 @@ package_name = 'sensor_simulator'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
+    packages=find_packages(exclude=['test']),    data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
@@ -23,7 +22,9 @@ setup(
         ],
     },
     entry_points={
-        'console_scripts': [
-        ],
-    },
+    'console_scripts': [
+        'temperature_publisher = sensor_simulator.temperature_publisher:main',
+        'temperature_subscriber = sensor_simulator.temperature_subscriber:main',
+    ],
+},
 )
